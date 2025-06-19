a69ec3e7fdf2f7d9f4e58d4ff979842a61e2cc52
This-Is-Prince <this.is.kumar.prince@gmail.com>
2025-06-19 12:06:45 +0530

    feat: string operator

---
+++ neso-academy/strings-in-python/string-operator.py
@@ -0,0 +1,28 @@
+z = 'a' + 'b' + 'c'
+
+print(z)
+
+x = 'I'
+y = 'am'
+z = 'Prince'
+
+print(x + ' ' + y + ' ' + z)
+
+# String Repetition Operator
+z = 'abc ' * 3
+print(z)
+z = 3 * 'abc '
+print(z)
+z = 0 * 'abc '
+print(z)
+
+print('Prin' in "Prince")
+print('prin' not in 'Prince')
+
+age = 24
+x = "My age is %d" % age
+
+print(x)
+
+a = "I got {0:.2f}% marks in English.".format(55.757453)
+print(a)
