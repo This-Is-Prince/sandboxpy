f8554cc33015c9488f081ad0ea5eb900642b96fa
This-Is-Prince <this.is.kumar.prince@gmail.com>
2025-06-19 10:26:04 +0530

    feat: strings

---
+++ neso-academy/strings-in-python/strings.py
@@ -0,0 +1,13 @@
+name = "Prince"
+
+statement = "The language 'Python' is named after Monty Python, not the snake."
+# statement2 = "Hello! "I am Print". How's your mother?" // SyntaxError: invalid syntax
+statement2 = "Hello! \"I am Prince\". How\'s your mother?"
+
+# Multiline string
+statement = """Hello World!
+This is Prince.
+How are you?
+"""
+
+print(statement)
