69aebe14ebb7fa0a5cffdb428d8a45ab956f5158
This-Is-Prince <this.is.kumar.prince@gmail.com>
2025-06-19 10:33:45 +0530

    feat: accessing string characters

---
+++ neso-academy/strings-in-python/accessing-string-characters.py
@@ -0,0 +1,10 @@
+statement = "HELLO"
+print(statement[0])
+
+# Accessing individual characters from the last
+print(statement[-4])
+
+# Accessing the substring of a string
+print(statement[1:7])  # This will print from index 1 to index 6 (not inclusive of 7)
+print(statement[0:5])
+print(statement[-4:-1])
