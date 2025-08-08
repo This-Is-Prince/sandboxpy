9d51678f65133108f3dbeb1f4ec8027d7128f95d
This-Is-Prince <this.is.kumar.prince@gmail.com>
2025-08-08 17:38:11 +0530

    feat: modules

---
+++ pystudio/my_project/calculator/basic_ops.py
@@ -0,0 +1,4 @@
+add = lambda a, b: a + b
+
+def subtract(a, b):
+    return a - b
---
+++ pystudio/my_project/main.py
@@ -0,0 +1,21 @@
+# from calculator import basic_ops
+# from calculator import constants
+
+# result = basic_ops.add(10, 2)
+# print(result)
+# print(constants.PI)
+
+# from calculator.basic_ops import add
+# from calculator.constants import PI
+
+# result = add(10, 2)
+# print(result)
+# print(PI)
+
+from calculator import basic_ops as ops
+from calculator import constants as const
+
+
+result = ops.subtract(2, 4)
+print(const.PI)
+print(result)
