507109f40ced00942988735fa16ba83e9474d6e4
This-Is-Prince <this.is.kumar.prince@gmail.com>
2025-09-03 16:10:17 +0530

    feat: functions

---
+++ peakbootcamp/05_functions/12_built_in.py
@@ -0,0 +1,8 @@
+def chai_flavor(flavor="masala"):
+    """Return the flavor of chai."""
+    return flavor
+
+print(chai_flavor.__doc__)
+print(chai_flavor.__name__)
+
+help(len)
---
+++ peakbootcamp/06_chai_business/main.py
@@ -0,0 +1,12 @@
+# import recipes.flavors
+
+# print(recipes.flavors.ginger_chai())
+
+# from recipes.flavors import ginger_chai, elachai_chai
+
+# ginger_chai()
+# elachai_chai()
+
+from .recipes.flavors import ginger_chai
+
+ginger_chai()
